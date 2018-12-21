d8200e5dae61650e614e42bda783bf775089f50d
Aqhmal Hafizi <nexcross99@gmail.com>
2018-12-21 15:39:39 +0800

    code comment**

--- dns_check.py
+++ dns_check.py
@@ -2,10 +2,13 @@
 import sys
 import dns.resolver
 
+# Show error if user doesn't insert domain
 if len(sys.argv) <= 1:
     print('Please insert your domain')
+    # Exit the program
     exit()
 
+# Get user input
 domain = sys.argv[1]
 
 print('DNS Records for :', domain)
@@ -13,14 +16,18 @@ print('DNS Records for :', domain)
 # Common DNS Record Type
 types = ['A', 'AAAA', 'CNAME', 'MX', 'TXT', 'NS']
 
+# Do loop for all dns record types
 for type in types:
     try:
+        # Query DNS Record
         records = dns.resolver.query(domain, type)
         # Only show record if there are record
         if len(records) > 0:
             print('\n' + type, 'Record :-')
+            # Loop and print records
             for key, value in enumerate(records):
                 print(key + 1, '=>', value)
     except:
+        # Error handler
         # print('\nUnable to retrieve', type, 'records')
         pass
